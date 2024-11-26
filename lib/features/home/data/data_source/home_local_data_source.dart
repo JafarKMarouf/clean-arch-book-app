c14809acad00a4a87b4bb0f5a03247a6f831f8bd
JafarKMarouf <jafar.k.marouf@gmail.com>
2024-11-26 18:33:56 +0300

    fetch featured and newest books from local database

--- lib/features/home/data/data_source/home_local_data_source.dart
+++ lib/features/home/data/data_source/home_local_data_source.dart
@@ -4,3 +4,17 @@ abstract class HomeLocalDataSource {
   List<BookEntity> fetchFeaturedBooks();
   List<BookEntity> fetchNewestBooks();
 }
+
+class HomeLocalDataSourceImp extends HomeLocalDataSource {
+  @override
+  List<BookEntity> fetchFeaturedBooks() {
+    var books = Hive.box<BookEntity>(kFeaturedBox);
+    return books.values.toList();
+  }
+
+  @override
+  List<BookEntity> fetchNewestBooks() {
+    var books = Hive.box<BookEntity>(kNewestBox);
+    return books.values.toList();
+  }
+}
